9066f9e48af82b00964969296179b818a28b2d0a
Aaron O'Donnell <S00171335@mail.itsligo.ie>
2018-10-16 21:35:14 +0100

    Must pass players through into Player class and assign them each an index

--- 2 Controller Attempt/Game1.cs
+++ 2 Controller Attempt/Game1.cs
@@ -3,6 +3,7 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Collections.Generic;
 
 namespace _2_Controller_Attempt
 {
@@ -12,11 +13,11 @@ namespace _2_Controller_Attempt
         SpriteBatch spriteBatch;
 
         SpriteFont font;
-        Vector2 player1Position;
-        Vector2 player2Position;
-        float speed;
+        public Player player, player1, player2, player3, player4;
 
-        //PlayerIndex currentIndex;
+        public List<Player> playerList = new List<Player>();
+
+        PlayerIndex playerIndex;
 
         public Game1()
         {
@@ -24,6 +25,7 @@ namespace _2_Controller_Attempt
             Content.RootDirectory = "Content";
 
             new InputManager(this);
+            player = new Player(this);
         }
 
         protected override void Initialize()
@@ -33,10 +35,18 @@ namespace _2_Controller_Attempt
 
         protected override void LoadContent()
         {
-            player1Position = new Vector2(20, 20);
-            player2Position = new Vector2(120, 150);
-            speed = 10f;
             font = Content.Load<SpriteFont>("SystemFont");
+
+            player1 = new Player(this);
+            player2 = new Player(this);
+            player3 = new Player(this);
+            player4 = new Player(this);
+
+            playerList.Add(player1);
+            playerList.Add(player2);
+            playerList.Add(player3);
+            playerList.Add(player4);
+
             spriteBatch = new SpriteBatch(GraphicsDevice);
         }
 
@@ -50,121 +60,7 @@ namespace _2_Controller_Attempt
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            #region Player1 Controller
-            if(GamePad.GetState(PlayerIndex.One).DPad.Up == ButtonState.Pressed)
-            {
-                player1Position.Y += speed;
-            }
-            if (GamePad.GetState(PlayerIndex.One).DPad.Down == ButtonState.Pressed)
-            {
-                player1Position.Y -= speed;
-            }
-            if (GamePad.GetState(PlayerIndex.One).DPad.Right == ButtonState.Pressed)
-            {
-                player1Position.Y -= speed;
-            }
-            if (GamePad.GetState(PlayerIndex.One).DPad.Left == ButtonState.Pressed)
-            {
-                player1Position.Y += speed;
-            }
-            #region Using Input Manager
-            //if(InputManager.IsButtonPressed(Buttons.DPadRight, PlayerIndex.One))
-            //{
-            //    player1Position.X += speed;
-            //}
-            //if (InputManager.IsButtonPressed(Buttons.DPadLeft, PlayerIndex.One))
-            //{
-            //    player1Position.X -= speed;
-            //}
-            //if (InputManager.IsButtonPressed(Buttons.DPadDown, PlayerIndex.One))
-            //{
-            //    player1Position.Y -= speed;
-            //}
-            //if (InputManager.IsButtonPressed(Buttons.DPadUp, PlayerIndex.One))
-            //{
-            //    player1Position.Y += speed;
-            //}
-            #endregion
-            #endregion
-
-            #region Player2 Controller
-            if (GamePad.GetState(PlayerIndex.Two).DPad.Up == ButtonState.Pressed)
-            {
-                player2Position.Y += speed;
-            }
-            if (GamePad.GetState(PlayerIndex.Two).DPad.Down == ButtonState.Pressed)
-            {
-                player2Position.Y -= speed;
-            }
-            if (GamePad.GetState(PlayerIndex.Two).DPad.Right == ButtonState.Pressed)
-            {
-                player2Position.Y -= speed;
-            }
-            if (GamePad.GetState(PlayerIndex.Two).DPad.Left == ButtonState.Pressed)
-            {
-                player2Position.Y += speed;
-            }
-
-            #region Using Input Manager
-            //if (InputManager.IsButtonPressed(Buttons.DPadRight, PlayerIndex.Two))
-            //{
-            //    player1Position.X += speed;
-            //}
-            //if (InputManager.IsButtonPressed(Buttons.DPadLeft, PlayerIndex.Two))
-            //{
-            //    player1Position.X -= speed;
-            //}
-            //if (InputManager.IsButtonPressed(Buttons.DPadUp, PlayerIndex.Two))
-            //{
-            //    player1Position.Y -= speed;
-            //}
-            //if (InputManager.IsButtonPressed(Buttons.DPadDown, PlayerIndex.Two))
-            //{
-            //    player1Position.Y += speed;
-            //}
-            #endregion
-            #endregion
-
-
-            #region Player1 Keyboard
-            //Using Input Manager
-            if (InputManager.IsKeyHeld(Keys.W))
-            {
-                player1Position.Y -= speed;
-            }
-            if (InputManager.IsKeyHeld(Keys.S))
-            {
-                player1Position.Y += speed;
-            }
-            if (InputManager.IsKeyHeld(Keys.A))
-            {
-                player1Position.X -= speed;
-            }
-            if (InputManager.IsKeyHeld(Keys.D))
-            {
-                player1Position.X += speed;
-            }
-            #endregion
-
-            #region Player2 Keyboard
-            //Using Input Manager
-            if (InputManager.IsKeyHeld(Keys.Up))
-            {
-                player2Position.Y -= speed;
-            }
-            if (InputManager.IsKeyHeld(Keys.Down))
-            {
-                player2Position.Y += speed;
-            }
-            if (InputManager.IsKeyHeld(Keys.Right))
-            {
-                player2Position.X += speed;
-            }
-            if (InputManager.IsKeyHeld(Keys.Left))
-            {
-                player2Position.X -= speed;
-            }
-            #endregion
+            player.Update(gameTime, playerIndex);
 
             // TODO: Add your update logic here
 
@@ -173,15 +69,9 @@ namespace _2_Controller_Attempt
 
         protected override void Draw(GameTime gameTime)
         {
-            //Console.WriteLine("Hi");
-            //Console.ReadLine();
             GraphicsDevice.Clear(Color.White);
 
-            spriteBatch.Begin();
-            spriteBatch.DrawString(font, "Player1", player1Position, Color.Red);
-            spriteBatch.DrawString(font, "Player2", player2Position, Color.Blue);
-            spriteBatch.End();
-            // TODO: Add your drawing code here
+            player.Draw(gameTime, font, spriteBatch);
 
             base.Draw(gameTime);
         }
---
+++ 2 Controller Attempt/Player.cs
@@ -0,0 +1,180 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Managers;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace _2_Controller_Attempt
+{
+    public sealed class Player : GameComponent
+    {
+        public PlayerIndex index;
+
+        int playerNumber = 0;
+
+        public List<Player> playerList = new List<Player>();
+
+        private static Vector2 prevPosition;
+        private static Vector2 currentPosition;
+
+        private static Vector2 P2prevPosition;
+        private static Vector2 P2currentPosition;
+
+        Player player;
+
+        //SpriteBatch _spritebatch;
+        //GraphicsDevice _graphics;
+
+        private float speed = 15f;
+
+        public Player(Game _game)
+            : base (_game)
+        {
+            GamePad.GetState(index);
+            _game.Components.Add(this);
+        }
+
+        public void GetPlayerPosition(Vector2 pos)
+        {
+            currentPosition = new Vector2(25, 25);
+            P2currentPosition = new Vector2(150, 150);
+        }
+
+        public void IsControllerDetected(List<Player> tempList)
+        {
+            GamePadState state = GamePad.GetState(PlayerIndex.One);
+            GamePadState state2 = GamePad.GetState(PlayerIndex.Two);
+            GamePadState state3 = GamePad.GetState(PlayerIndex.Three);
+            GamePadState state4 = GamePad.GetState(PlayerIndex.Four);
+
+            GetPlayerIndex(tempList, player);
+
+            #region Controller Detection
+
+            //if (state.IsConnected)
+            //{
+            //    playerList.Add(player1);
+            //}
+            //if (state2.IsConnected)
+            //{
+            //    playerList.Add(player2);
+            //}
+            //if (state3.IsConnected)
+            //{
+            //    playerList.Add(player3);
+            //}
+            //if (state4.IsConnected)
+            //{
+            //    playerList.Add(player4);
+            //}
+
+            #endregion
+        }
+
+        public void GetPlayerIndex(List<Player> playersList, Player tempPlayer)
+        {
+
+        }
+
+        public void Update(GameTime gameTime, PlayerIndex player, List<Player> playersList)
+        {
+            prevPosition = currentPosition;
+            P2prevPosition = P2currentPosition;
+
+            IsControllerDetected(playersList);
+
+            //GamePadState state = GamePad.GetState(PlayerIndex.One);
+            //GamePadState state2 = GamePad.GetState(PlayerIndex.Two);
+            //GamePadState state3 = GamePad.GetState(PlayerIndex.Three);
+            //GamePadState state4 = GamePad.GetState(PlayerIndex.Four);
+
+            #region Player Counts for Indexing
+
+            //if (playerList.Count <= 1)
+            //{
+            //    playerNumber = 1;
+            //    player = PlayerIndex.One;
+            //}
+            //else if (playerList.Count == 2)
+            //{
+            //    playerNumber = 2;
+            //    player = PlayerIndex.Two;
+            //}
+            //else if (playerList.Count == 3)
+            //{
+            //    playerNumber = 3;
+            //    player = PlayerIndex.Three;
+            //}
+            //else if (playerList.Count == 4)
+            //{
+            //    playerNumber = 4;
+            //    player = PlayerIndex.Four;
+            //}
+            #endregion
+
+            #region Player1 Controller
+            foreach (Player players in playersList)
+            {
+                if (InputManager.IsButtonPressed(Buttons.DPadRight, players.index))
+                {
+                    currentPosition.X += speed;
+                }
+                if (InputManager.IsButtonPressed(Buttons.DPadLeft, players.index))
+                {
+                    currentPosition.X -= speed;
+                }
+                if (InputManager.IsButtonPressed(Buttons.DPadDown, players.index))
+                {
+                    currentPosition.Y += speed;
+                }
+                if (InputManager.IsButtonPressed(Buttons.DPadUp, players.index))
+                {
+                    currentPosition.Y -= speed;
+                }
+            }
+            
+                #endregion
+
+            //else if (player == PlayerIndex.Two)
+            //{
+               #region Player2 Controller
+
+            //    //if (InputManager.IsButtonPressed(Buttons.DPadRight, player))
+            //    //{
+            //    //    currentPosition.X += speed;
+            //    //}
+            //    //if (InputManager.IsButtonPressed(Buttons.DPadLeft, player))
+            //    //{
+            //    //    currentPosition.X -= speed;
+            //    //}
+            //    //if (InputManager.IsButtonPressed(Buttons.DPadDown, player))
+            //    //{
+            //    //    currentPosition.Y += speed;
+            //    //}
+            //    //if (InputManager.IsButtonPressed(Buttons.DPadUp, player))
+            //    //{
+            //    //    currentPosition.Y -= speed;
+            //    //}
+               #endregion
+            //}
+        }
+
+        public void Draw(GameTime gameTime, SpriteFont font, SpriteBatch _spritebatch)
+        {
+            _spritebatch.Begin();
+
+            foreach (Player player in playerList)
+            {
+                //playerNumber++;
+                _spritebatch.DrawString(font, "Player" + playerNumber, currentPosition, Color.Red);
+            }
+            //_spritebatch.DrawString(font, "Player1", currentPosition, Color.Red);
+            //_spritebatch.DrawString(font, "Player2", P2currentPosition, Color.Blue);
+            _spritebatch.End();
+        }
+    }
+}
